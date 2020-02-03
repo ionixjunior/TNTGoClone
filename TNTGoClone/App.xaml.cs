a4eb7e23c395a61f06a0fb2468613a174ab0ebe3
Ione Souza Junior <junior@ionixjunior.com.br>
2020-02-03 19:59:55 -0300

    Adicionar tela principal no namespace Views

--- TNTGoClone/App.xaml.cs
+++ TNTGoClone/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TNTGoClone.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace TNTGoClone
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new MainView();
 		}
 
 		protected override void OnStart()
