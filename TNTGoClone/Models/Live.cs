09129276105d4c8ee690f0645c9bb3fa133b032d
Ione Souza Junior <junior@ionixjunior.com.br>
2020-02-03 20:36:16 -0300

    Mapear classe com a estrutura das lives

---
+++ TNTGoClone/Models/Live.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TNTGoClone.Models
+{
+    public class Live
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("logo")]
+        public string Logo { get; set; }
+
+        [JsonProperty("background")]
+        public string Background { get; set; }
+
+        [JsonProperty("is_locked")]
+        public bool IsLocked { get; set; }
+    }
+}
