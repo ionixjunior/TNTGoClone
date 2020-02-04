88207d499b5cd441bf0bc6816d1d8d9686d3f506
Ione Souza Junior <junior@ionixjunior.com.br>
2020-02-03 21:33:31 -0300

    Mapear classe com estrutura para filmes

---
+++ TNTGoClone/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TNTGoClone.Models
+{
+    public class Movie
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("poster")]
+        public string Poster { get; set; }
+
+        [JsonProperty("channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty("is_locked")]
+        public bool IsLocked { get; set; }
+    }
+}
