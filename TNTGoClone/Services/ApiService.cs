26d327871c24f90b66cfae605f66f405efbf4274
Ione Souza Junior <junior@ionixjunior.com.br>
2020-02-03 21:39:46 -0300

    Implementar consumo da API com Refit

---
+++ TNTGoClone/Services/ApiService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Refit;
+using TNTGoClone.Interfaces;
+using TNTGoClone.Models;
+
+namespace TNTGoClone.Services
+{
+	public class ApiService : IApi
+	{
+		private static ApiService _instance;
+		public static ApiService Instance = _instance ?? (_instance = new ApiService());
+
+		private readonly IApi _api;
+
+		private ApiService()
+		{
+			_api = RestService.For<IApi>("https://tntgo.getsandbox.com");
+		}
+
+		public async Task<IList<Live>> GetLives()
+		{
+			return await _api.GetLives();
+		}
+
+		public async Task<IList<Movie>> GetMovies()
+		{
+			return await _api.GetMovies();
+		}
+	}
+}
