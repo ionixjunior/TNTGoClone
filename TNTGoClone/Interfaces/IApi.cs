3dd1b101d0854101fda1d2dc2263031aa76dff5e
Ione Souza Junior <junior@ionixjunior.com.br>
2020-02-03 21:36:02 -0300

    Mapear interface da API com o Refit

---
+++ TNTGoClone/Interfaces/IApi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Refit;
+using TNTGoClone.Models;
+
+namespace TNTGoClone.Interfaces
+{
+	public interface IApi
+	{
+		[Get("/live")]
+		Task<IList<Live>> GetLives();
+
+		[Get("/movie")]
+		Task<IList<Movie>> GetMovies();
+	}
+}
