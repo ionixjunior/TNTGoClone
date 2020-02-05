9d8e2ad7343d91c8e943814762d1ccdcb7aae381
Ione Souza Junior <junior@ionixjunior.com.br>
2020-02-05 13:03:40 -0300

    Implementar carregamento das lives

--- TNTGoClone/ViewModels/MainViewModel.cs
+++ TNTGoClone/ViewModels/MainViewModel.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MvvmHelpers;
+using TNTGoClone.Interfaces;
 using TNTGoClone.Models;
+using TNTGoClone.Services;
 
 namespace TNTGoClone.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        private readonly IApi _api;
+
         public IList<AppPage> Pages { get; private set; }
+        public ObservableRangeCollection<Live> Lives { get; private set; }
 
         public MainViewModel()
         {
+            _api = ApiService.Instance;
+
             Pages = GetPages();
+            Lives = new ObservableRangeCollection<Live>();
         }
 
         private IList<AppPage> GetPages()
@@ -28,6 +36,20 @@ namespace TNTGoClone.ViewModels
 
         public async Task InitializeAsync()
         {
+            await LoadLives();
+        }
+
+        private async Task LoadLives()
+        {
+            try
+            {
+                var lives = await _api.GetLives();
+                Lives.AddRange(lives);
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception.Message);
+            }
         }
     }
 }
