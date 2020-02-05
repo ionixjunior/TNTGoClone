02ae4cac34afbdc255c8c52ed531f7a5c7e21404
Ione Souza Junior <junior@ionixjunior.com.br>
2020-02-05 13:19:54 -0300

    Implementar data template selector para as páginas do menu

---
+++ TNTGoClone/Templates/AppPageDataTemplateSelector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TNTGoClone.ContentViews;
+using TNTGoClone.Models;
+using Xamarin.Forms;
+
+namespace TNTGoClone.Templates
+{
+    public class AppPageDataTemplateSelector : DataTemplateSelector
+    {
+        private readonly DataTemplate _live;
+        private readonly DataTemplate _movie;
+        private readonly DataTemplate _show;
+        private readonly DataTemplate _extra;
+
+        public AppPageDataTemplateSelector()
+        {
+            _live = new DataTemplate(typeof(LiveContentView));
+            _movie = new DataTemplate(typeof(MovieContentView));
+            _show = new DataTemplate(typeof(ShowContentView));
+            _extra = new DataTemplate(typeof(ExtraContentView));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is AppPage appPage)
+            {
+                if (appPage.Type == AppPageType.Live)
+                    return _live;
+
+                if (appPage.Type == AppPageType.Movie)
+                    return _movie;
+
+                if (appPage.Type == AppPageType.Show)
+                    return _show;
+
+                if (appPage.Type == AppPageType.Extra)
+                    return _extra;
+
+                return new DataTemplate(typeof(ContentView));
+            }
+
+            return new DataTemplate(typeof(ContentView));
+        }
+    }
+}
