0e7f354cc818a65f31a2e39aa413a77640820ff0
Ione Souza Junior <junior@ionixjunior.com.br>
2020-03-01 16:30:47 -0300

    Implementar viewmodel dos extras

---
+++ TNTGoClone/ViewModels/ExtraViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using MvvmHelpers;
+using TNTGoClone.Interfaces;
+using TNTGoClone.Models;
+
+namespace TNTGoClone.ViewModels
+{
+    public class ExtraViewModel
+    {
+        private readonly IApi _api;
+        private bool _alreadyInitialized = false;
+
+        public ObservableRangeCollection<Extra> Extras { get; private set; }
+
+        public ExtraViewModel(IApi api)
+        {
+            _api = api;
+            Extras = new ObservableRangeCollection<Extra>();
+        }
+
+        public async Task InitializeAsync()
+        {
+            if (_alreadyInitialized)
+                return;
+
+            await GetExtras();
+            _alreadyInitialized = true;
+        }
+
+        private async Task GetExtras()
+        {
+            try
+            {
+                var extras = await _api.GetExtras();
+                Extras.AddRange(extras);
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception.Message);
+            }
+        }
+    }
+}
